a570a0703767a3d6490504f9c2f28345c4fdbb0f
jungsungyoon <dkffprtm0404@naver.com>
2021-07-04 13:28:52 +0000

    Dibuging

--- XHelp1082.cpp
+++ XHelp1082.cpp
@@ -11,13 +11,18 @@ int main() {
   for(int i=0;i<N;i++)
   {
     cin >> arr[i];
-    if(min>arr[i]) //첫번쨰자리 고정 0원이다
+    if(min>arr[i]) //첫번쨰자리 고정 0원이다 
     {
+		
       min=arr[i];
+		//cout<<arr[i]<<'\n';
     }
     if(i>0&&min2>arr[i]) //2번째 자리 1원이다
     {
+		//cout << arr[i];
       min2=arr[i];
+		//cout<<min2<<'\n';
+    	
     }
   }
   cin>>M;
@@ -27,20 +32,21 @@ int main() {
   }
   else
   {
-    num=((M-min2)/min)+1;  // 만들어야할 자리수
+    num=((M-min2)/min)+1;  // 만들어야할 자리수 21-7 14/6= 2+1 3
   
   int t=num;
-  for(int j=0;j<num;j++)
+  for(int j=0;j<num;j++) //0 1
   {
-    for(int i=N-1;i>-1;i--)
+    for(int i=N-1;i>-1;i--) //i=2  i=1
     {
-      if(M-arr[i]<0)
+		
+      if(M-arr[i]<0) //21-8<0 f 21-7<0 f
         continue;
-      if(t==((M-arr[i])/min+1))
+      if(t==((M-arr[i])/min+1)) //13/6 2+1 3T
       {
-        M-=arr[i];
-        t--;
-        ans[j]=i;
+        M-=arr[i];//13
+        t--;  //2
+        ans[j]=i;  //2 저장
         break;
       }
     }
